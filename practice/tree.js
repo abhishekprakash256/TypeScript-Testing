0e14b7f9e598f77dc35cd3f0e364cfe84c795fc6
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-01-29 15:54:52 -0500

    code

---
+++ practice/tree.js
@@ -0,0 +1,71 @@
+/*
+code to make tree and do the traversal
+*/
+// maek the TreeNode class
+var TreeNode = /** @class */ (function () {
+    function TreeNode(val, left, right) {
+        if (left === void 0) { left = null; }
+        if (right === void 0) { right = null; }
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+    return TreeNode;
+}());
+// Create the nodes
+var root = new TreeNode(1);
+var node2 = new TreeNode(2);
+var node3 = new TreeNode(3);
+var node4 = new TreeNode(4);
+var node5 = new TreeNode(5);
+var node6 = new TreeNode(6);
+var node7 = new TreeNode(7);
+// Attach the nodes correctly
+root.left = node2;
+root.right = node3;
+node2.left = node4;
+node2.right = node5;
+node3.left = node6;
+node3.right = node7;
+/*
+Tree image
+
+        1
+       / \
+      2   3
+     / \  / \
+    4   5 6  7
+
+
+*/
+//make the helper function to traverse the tree
+var Helper_Fun = /** @class */ (function () {
+    function Helper_Fun() {
+    }
+    Helper_Fun.prototype.helper_dfs = function (node) {
+        //base case 
+        if (!node) {
+            return;
+        }
+        //print the value
+        console.log(node.val);
+        //make the recursive call
+        this.helper_dfs(node.left);
+        this.helper_dfs(node.right);
+    };
+    Helper_Fun.prototype.tree_print_dfs = function (root) {
+        //base case 
+        if (!root) {
+            console.log(null);
+        }
+        if (root.left == null && root.right == null) {
+            console.log(root.val);
+        }
+        //make the recurseive call
+        this.helper_dfs(root);
+    };
+    return Helper_Fun;
+}());
+//make the helper function 
+var helper_fun = new Helper_Fun();
+console.log(helper_fun.tree_print_dfs(root));
--- practice/tree.ts
+++ practice/tree.ts
@@ -2,3 +2,100 @@
 code to make tree and do the traversal
 */
 
+
+// maek the TreeNode class
+class TreeNode
+{
+	val : number ; 
+	left : TreeNode | null ; 
+	right : TreeNode | null ;
+
+
+    constructor(val: number , left : TreeNode | null = null , right : TreeNode | null = null ) {
+        this.val = val ;
+        this.left = left ; 
+        this.right = right ;
+
+    }
+}
+
+
+// Create the nodes
+const root = new TreeNode(1);
+const node2 = new TreeNode(2);
+const node3 = new TreeNode(3);
+const node4 = new TreeNode(4);
+const node5 = new TreeNode(5);
+const node6 = new TreeNode(6);
+const node7 = new TreeNode(7);
+
+
+// Attach the nodes correctly
+root.left = node2;
+root.right = node3;
+node2.left = node4;
+node2.right = node5;
+node3.left = node6;
+node3.right = node7;
+
+
+/*
+Tree image
+
+        1
+       / \
+      2   3
+     / \  / \
+    4   5 6  7
+
+
+*/
+
+//make the helper function to traverse the tree
+
+class Helper_Fun
+{	
+	helper_dfs(node: any)
+	{
+
+	//base case 
+	if (! node) 
+		{
+			return ;
+		}
+
+		//print the value
+		console.log(node.val) ;
+
+		//make the recursive call
+		this.helper_dfs(node.left) ; 
+
+		this.helper_dfs(node.right) ;
+
+	}
+
+	tree_print_dfs( root : any )
+	
+	{
+		//base case 
+		if (! root)
+			{
+				console.log(null) ;
+			} 
+
+		if (root.left == null && root.right == null)
+			{
+				console.log(root.val) ;
+			}
+
+		//make the recurseive call
+		this.helper_dfs(root) ;
+
+	} 
+}
+
+
+
+//make the helper function 
+const helper_fun = new Helper_Fun() ;
+console.log(helper_fun.tree_print_dfs(root)) 
